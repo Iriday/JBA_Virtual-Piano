b74ee191952cedd74abe508d8bf922fa3844513b
Igor <GI_Something@protonmail.com>
2020-06-17 19:54:02 +0300

    feat: Add audio files. Play audio when key is pressed/clicked (white keys)

---
+++ src/piano/listeners.js
@@ -0,0 +1,20 @@
+const keys = "ASDFGHJ"
+const pathAudioBgn = "audio/"
+const pathAudioEnd = ".mp3"
+
+for (let k in keys) {
+    document.getElementById(keys[k]).addEventListener("click", function () {
+        playSound(pathAudioBgn + keys[k] + pathAudioEnd)
+    });
+}
+
+document.addEventListener("keydown", function (event) {
+    const key = event.key.toUpperCase()
+    if (keys.includes(key)) {
+        playSound(pathAudioBgn + key + pathAudioEnd)
+    }
+})
+
+function playSound(filePath) {
+    new Audio(filePath).play()
+}
